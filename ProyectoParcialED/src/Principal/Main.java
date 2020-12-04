028c41d2d1e3431618a37d4f20358fbd56e55436
jcotrina2000 <jcotrina@espol.edu.ec>
2020-12-04 02:52:14 -0500

    Main

---
+++ ProyectoParcialED/src/Principal/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+import Modelo.View;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+/**
+ *
+ * @author User
+ */
+public class Main extends Application{
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        View view=new View();
+        Scene scene=view.iniciarEscena();
+        primaryStage.setScene(scene);
+        scene.getStylesheets().add("estilo/estilo.css");
+        primaryStage.setTitle("El problema de Josefo");
+        primaryStage.show();
+    }
+    
+}
